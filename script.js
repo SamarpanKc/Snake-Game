ee425f0428a5977a245bc803257cc909c4c266b0
Samarpan KC <samarpan0kc@gmail.com>
2024-10-27 14:37:16 +0545

    JI

--- script.js
+++ script.js
@@ -4,14 +4,9 @@ const filler = board.getContext("2d");
 const sqr = 40;
 const box = 40;
 
-
 const snake = [
   { x: 5 * box, y: 5 * box },
   { x: 6 * box, y: 5 * box },
-  { x: 7 * box, y: 5 * box },
-  { x: 8 * box, y: 5 * box },
-  { x: 9 * box, y: 5 * box },
-  { x: 10 * box, y: 5 * box },
 ];
 
 let direction = "LEFT";
@@ -28,3 +23,14 @@ function updating() {
   snake.unshift(head); //naya tauko chaged posion ma jodeko
   snake.pop();
 }
+
+function drawing() {
+  filler.clearRect(0, 0, board.width, board.height); // Clear canvas
+
+  for (let i = 0; i < snake.length; i++) {
+    filler.fillStyle = i === 0 ? "black" : "white";
+    filler.fillRect(snake[i].x, snake[i].y, sqr, sqr);
+  }
+}
+
+drawing();
