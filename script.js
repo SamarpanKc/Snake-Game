d2e500f592b3557cf619084f0fdd17fa1d3c64cb
Sumit Chhetri <sumitchhetri277@gmail.com>
2024-10-28 13:12:20 +0545

    push

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 const board = document.getElementById("board");
 const filler = board.getContext("2d");
 
-const sqr = 40;
-const box = 40;
+const sqr = 20;
+const box = 20;
 
 const snake = [
   { x: 5 * box, y: 5 * box },
   { x: 6 * box, y: 5 * box },
 ];
 
-let direction = "LEFT";
+let direction = "RIGHT";
 let game;
+let isGameOver = false;
 
 function updating() {
   const head = { ...snake[0] }; //naya tauko to add infront of the changed position
@@ -20,6 +21,19 @@ function updating() {
   if (direction === "RIGHT") head.x += sqr;
   if (direction === "DOWN") head.y += sqr;
 
+  function isOutOfBounds(position) {
+    return (
+      position.x < 0 ||
+      position.x >= board.width ||
+      position.y < 0 ||
+      position.y >= board.height
+    );
+  }
+
+  if (isOutOfBounds(head)) {
+    gameOver();
+    return;
+  }
   snake.unshift(head); //naya tauko chaged posion ma jodeko
   snake.pop();
 }
@@ -58,4 +72,32 @@ function drawing() {
   }
 }
 
-drawing();
+function loop() {
+  updating();
+  drawing();
+}
+
+function gameOver() {
+  clearInterval(game);
+  isGameOver = true;
+  document.getElementById("message").textContent = "GAME OVER";
+  document.getElementById("btnRestart").style.display = "block"; // Show restart button
+  //   document.getElementById("btn").style.opacity = "1"; // Show start button again
+}
+//////////////////////////////////////
+
+function startGame() {
+  if (isGameOver) {
+    // Reset game state
+    snake.length = 2;
+    snake[0] = { x: 5 * box, y: 5 * box };
+    snake[1] = { x: 6 * box, y: 5 * box };
+    direction = "LEFT";
+    isGameOver = false;
+    document.getElementById("message").textContent = "";
+  }
+
+  document.getElementById("btn").style.opacity = "0"; // Hide start button
+  game = setInterval(loop, 200);
+  document.addEventListener("keydown", movement);
+}
